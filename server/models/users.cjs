89dfdda2747963a0ee5284039ffc8f9a7a4e7562
André Casimiro <andrecasimio@gmail.com>
2025-11-12 20:16:54 -0300

    create the database scheme and configuration for the postgres

---
+++ server/models/users.cjs
@@ -0,0 +1,52 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    static associate(models) {
+      users.belongsToMany(models.locations, { foreignKey: 'userId', as: 'fvLocations', through: 'favoriteLocations' });
+    }
+  }
+  users.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 255]
+      }
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+      onUpdate: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'users',
+    tableName: 'users',
+    timestamps: true
+  });
+  return users;
+};
