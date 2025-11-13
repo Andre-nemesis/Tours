0af828a7c705a635634724f01cfe1d8ba71d308d
André Casimiro <andrecasimio@gmail.com>
2025-11-13 08:55:10 -0300

    config the native project to change the form to how is inicializible

---
+++ tours/app/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
