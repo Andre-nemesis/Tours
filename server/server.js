ea39834c83cf4e5a4c8a6f18a47a1de9d1730122
André Casimiro <andrecasimio@gmail.com>
2025-11-14 16:49:22 -0300

    add location controller

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import sequelize from './config/db.js';
 
 dotenv.config();
 
@@ -11,13 +10,6 @@ const PORT = process.env.PORT || 3000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
-// Sincronize modelos (apenas em dev; use migrations em prod)
-if (process.env.NODE_ENV !== 'production') {
-  sequelize.sync({ force: false }) // force: true apaga e recria tabelas
-    .then(() => console.log('Modelos sincronizados!'))
-    .catch(err => console.error('Erro na sincronização:', err));
-}
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
